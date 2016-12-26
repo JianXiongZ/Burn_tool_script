60c18719143927843767c002607468dfa20ae27a
jianxiong <jianxiong@canaan-creative.com>
2016-12-26 12:09:23 +0800

    rename script

--- BurnAvalon7_mm.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-cd /home/Burn_tool_script
-while true; do 
-        make isedir=/home/factory/Xilinx/14.6/ISE_DS reflash;
-echo;
-read -p "Press any key to burn next"
-done
--- BurnAvalon7_mm2.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-cd ~/Burn_tool_script
-while true; do 
-        make isedir=/home/factory/Xilinx/14.6/ISE_DS reflash_2;
-echo;
-read -p "Press any key to burn next"
-done
